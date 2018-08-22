7fdda7fb5f51782bdbb5f1c32ba3d241c985ab94
lucidobservor <fmcsuy@gmail.com>
2018-08-22 09:45:00 -0400

    updated advancements to award trophies

--- util/src/main/java/floramancer/util/AdvancementGenerator.java
+++ util/src/main/java/floramancer/util/AdvancementGenerator.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public class AdvancementGenerator {
 
-    private static final String OUTPUT_DIRECTORY = "/util/output/flowerQuests/";
+    private static final String OUTPUT_DIRECTORY = "/util/output/";
     private static final int X_OFFSET = 4;
 
     public static void main(String[] args) throws IOException {
@@ -18,27 +18,30 @@ public class AdvancementGenerator {
         for (FlowerType flower : FlowerType.values()) {
             List<String> lines = new ArrayList<>();
 
+            String advancementName = String.format("\"advancements.floramancer.%s.title\"", flower.getBotaniaTweaksName());
+            String flowerItem = String.format("<botania:specialflower, nbt:{type: \"%s\"}>", flower.getBotaniaName());
+
+            String rewardItem = String.format("<simple_trophies:trophy, nbt:{" +
+                    "TrophyItem:{id:\"botania:specialflower\",tag:{type:\"%s\"}}," +
+                    "TrophyName:\"advancements.floramancer.%s.title\"}>",
+                    flower.getBotaniaName(), flower.getBotaniaTweaksName());
+
 			// <simple_trophies:trophy, meta:0, count:2, nbt:{TrophyItem:{id:"botania:specialflower",Count:1b,tag:{type:"hydroangeas"},Damage:0s},TrophyName:"advancements.floramancer.hydroangeas.title"}>
 			
             lines.add("");
-            lines.add(String.format("setIcon(<botania:specialflower, nbt:{type: \"%s\"}>)", flower.getBotaniaName()));
-            lines.add(String.format("setTitle(\"advancements.floramancer.%s.title\")", flower.getBotaniaTweaksName()));
+            lines.add(String.format("setIcon(%s)", flowerItem));
+            lines.add(String.format("setTitle(%s)", advancementName));
             lines.add(String.format("setDescription(\"Fill %d Mana Pools using the %s\")", flower.getPoolsRequired(), flower.getLocalizedName()));
             lines.add("addParent(\"triumph:floramancer/lexicon\")");
             lines.add("");
             lines.add(String.format("setPos(%d, 8)", X_OFFSET + flower.getId()));
             lines.add("drawDirectLines(true)");
             lines.add("");
-            lines.add(String.format("setRewardItem(\"contenttweaker:%strophy\")", flower.getBotaniaTweaksName()));
+            lines.add(String.format("setRewardItem(%s)", rewardItem));
             lines.add(String.format("addRewardFunction(\"triumph:stages/stage-%s\")", flower.getBotaniaTweaksName()));
             lines.add("");
 
-            // Botania Tweaks version
             lines.addAll(getPoolCriteria(flower));
-
-            // Vanilla Version
-            //lines.add(String.format("criteria = addCriteria(\"%s_acquire\", \"minecraft:inventory_changed\")", flower.getBotaniaName()));
-            //lines.add(String.format("criteria.addItem(<botania:specialflower, nbt:{type: \"%s\"}>)", flower.getBotaniaName()));
 
             Files.write(Paths.get(absolutePath + flower.getBotaniaTweaksName() + ".txt"), lines);
         }
